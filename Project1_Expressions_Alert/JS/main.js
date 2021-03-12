222369ac2ad9e2328597b609a94a64be87e9e855
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-12 10:57:37 -0500

    Initial commit

---
+++ Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,23 @@
+
+alert(newFunction());
+//This is concatenating two variables.
+var x = "HELLO";
+var y = "WORLD!<br>";
+var z = x + y;
+document.writeln(z);
+
+document.write("Tony told Dad!\" <br> \"I got an A on my report card!\"<br>\"Good Job!\"Dad responded.");
+
+document.write("\"Life is like a box of chocolates,"
+    + " You never know\ what you will get.");
+function newFunction() {
+    return "HELLO WORLD";
+}
+
+function My_First_Function() {
+        var str = "This text is green!";
+        var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+
+
---
+++ Project_Function_2/JS/main1.js
@@ -0,0 +1,11 @@
+function My_First_Function() {
+    var str = "The Red Tape";
+    var result = str.fontcolor("red");
+    document.getElementById("red_text").innerHTML = result;
+}  // JavaScript source code
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
