2d4fd9f944088e0f5b2490cd11a10988e0a14bb8
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-20 01:58:23 -0400

    Add files via upload

--- Project_Function_2/JS/main1.js
+++ Project_Function_2/JS/main1.js
@@ -2,7 +2,7 @@ function My_First_Function() {
     var str = "The Red Tape";
     var result = str.fontcolor("red");
     document.getElementById("red_text").innerHTML = result;
-}  // JavaScript source code
+}  // JavaScript function
 
 function myFunction() {
     var sentence = "I am learning";
