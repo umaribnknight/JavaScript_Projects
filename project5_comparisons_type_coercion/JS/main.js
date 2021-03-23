471121bc83573783def1da82bf58688e288fc3ee
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-23 14:28:07 -0400

    comparisons

--- project5_comparisons_type_coercion/JS/main.js
+++ project5_comparisons_type_coercion/JS/main.js
@@ -7,3 +7,10 @@ document.write(3 == 10);
 x = 10;
 y = 10;
 document.write(x === y);
+document.write(5 > 2 && 10 > 4);
+document.write(5 > 10 && 10 > 4);
+document.write(5 > 10 || 10 > 4);
+document.write(5 > 10 || 10 > 20);
+function not_Function()  {                       //The function used in the paragraph id
+    document.getElementById("Not") .innerHTML = !(20 > 10);
+}
