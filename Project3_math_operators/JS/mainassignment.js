ad307215b4b79aab34030fce70ec778e4d1817e5
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-23 10:29:25 -0400

    assignment

--- Project3_math_operators/JS/mainassignment.js
+++ Project3_math_operators/JS/mainassignment.js
@@ -1,4 +1,15 @@
-function multiplication() {               //This function is for multiplictation.
+function mult() {               //This function is for multiplictation.
     var simple_Math = 6 * 8;
-    document.getElementbyId("Math").innerHTML = "6 x 8 = " + simple_Math;
+    document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function modulus_Operator() {  //This function is for division with remainder
+    var simple_Math = 25 % 6;
+    document.getElementById("Modulus").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function more_Math() {
+    var simple_Math = (1+2) * 10 / 2 -5;
+    document.getElementById("More_Math").innerHTML = "1 plus 2, multiplied by 10, 
+    divided in half and then subtracted by 5 equals " + simple_math; 
 }
