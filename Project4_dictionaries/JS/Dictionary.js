f8f22495cfa4e43eca84e2fbdb97678a18074127
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-23 13:39:58 -0400

    updates

---
+++ Project4_dictionaries/JS/Dictionary.js
@@ -0,0 +1,12 @@
+function my_Dictionary() {                    //function name
+    var Animal = {                            //variables
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    delete Animal.sound                    // delete var
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+
+}
--- project5_comparisons_type_coercion/JS/main.js
+++ project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,6 @@
 document.write("10" + 5)
+document.write(10 > 2);
+document.write(10 < 2);
+console.log(2 +2);
+document.write(10 == 10);
+document.write(3 == 10);
