93e90e1c588590a88a5a2da58ac6a11cad7d5473
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-23 10:39:46 -0400

    assisnment

--- Project3_math_operators/function my_Dictionary() {.js
+++ Project3_math_operators/function my_Dictionary() {.js
@@ -1,12 +1,12 @@
-function my_Dictionary() {
-    var Animal = {
+function my_Dictionary() {                    //function name
+    var Animal = {                            //variables
         Species:"Dog",
         Color:"Black",
         Breed:"Labrador",
         Age:5,
         Sound:"Bark!"
     };
-    delete animal.sound
+    delete animal.sound                    // delete var
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 
 }
