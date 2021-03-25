bf5451becfee2f05e8ab8987faa8ea3521aa1d53
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-25 12:55:23 -0400

    project 6 index1

---
+++ main1.js
@@ -0,0 +1,9 @@
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus one() {Starting point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
