66dc9cb0c617ea60b7578c53df6c26c86e283e6d
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-27 09:41:04 -0400

    update

--- Project 6 assignment/JS/main.js
+++ Project 6 assignment/JS/main.js
@@ -11,7 +11,7 @@ function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height") .value;
     Can_ride = (Height < 52) ? "You are too short":"you are tall enough";
-    document.getElementByID("Ride").innerHTML = Can_ride + " to ride.";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
     }
 function Vehicle(Make, Model, Year, Color)  {
     this.Vehicle_Make = Make;
---
+++ Project 7/JS/main.js
@@ -0,0 +1,15 @@
+var x = 10;
+function Add_numbers_1() {
+    document.write(20 + x + "<br>");
+}
+function Add_numbers_2() {
+    document.write(20 + x + "<br>");
+}
+function Add_numbers_2() {
+    document.write(x + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+if (1 < 2) {
+    document.write("The left number is smaller than the number on the right.")
+}
