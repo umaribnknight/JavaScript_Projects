67a4230c0e10964886cbadd58ca76640355012ab
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-27 10:28:17 -0400

    Update main.js

--- Project 7/JS/main.js
+++ Project 7/JS/main.js
@@ -13,3 +13,12 @@ Add_numbers_2();
 if (1 < 2) {
     document.write("The left number is smaller than the number on the right.")
 }
+function Add_numbers_1() {
+    var x = 10;
+    console.log(15 + x);
+}
+function Add_numbers_2() {
+    console.log(x + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
