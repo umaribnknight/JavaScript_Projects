eb9497600a0edf6bde9537ea1cefe7ff871cd945
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-03-29 16:02:12 -0400

    update

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -2,9 +2,9 @@ function full_Sentence() {
    var part_1 = "I have ";
    var part_2  = "made this ";
    var part_3 = "into a complete ";
-   var part_4 = "sentence.",
+   var part_4 = "sentence.";
    var whole_sentence = part_1.concat(part_2, part_3, part_4);
-   document.getElementById("Concatenate").innerHTML = whole-sentence;
+   document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
 function slice_Method() {
     var Sentence = "All work and no play makes johnny a dukk boy.";
@@ -13,9 +13,9 @@ function slice_Method() {
 }
 function precision_Method() {
     var x = 12938.3012987376112;
-    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+    document.getElementById("Precision").innerHTML = x.toPrecision(10);
 }
 function string_Method() {
     var x =182;
-    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+    document.getElementById("Numbers_to_string").innerHTML = x.toString();
 }
