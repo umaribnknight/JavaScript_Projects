1b461e8c3a9890b117d63d4b5533794d66f35bc6
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-04-14 11:14:36 -0400

    update

--- one-page-website/JS/JavaScript.js
+++ one-page-website/JS/JavaScript.js
@@ -33,9 +33,10 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active",
+   "");
   }
-  slides[slideIndex-1].style.display ="block";
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
